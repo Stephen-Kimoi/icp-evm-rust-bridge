8e89964ccb07ef83477649263924a274363ba432
Elias Datler <eliasdatler@me.com>
2024-03-21 23:13:14 +0000

    Minor update

--- backend/src/lib.rs
+++ backend/src/lib.rs
@@ -2,15 +2,16 @@
 
 mod evm_rpc;
 use evm_rpc::{
-    Block, BlockTag, EvmRpcCanister, GetBlockByNumberResult, MultiGetBlockByNumberResult,
-    RpcServices,
+    Block, BlockTag, EthMainnetService, EvmRpcCanister, GetBlockByNumberResult,
+    MultiGetBlockByNumberResult, RpcServices,
 };
 
 #[ic_cdk::update]
 async fn getLatestEthereumBlock() -> Block {
     let cycles = 10_000_000_000;
+    let rpc_providers = vec![EthMainnetService::Cloudflare];
     let (result,) = EvmRpcCanister::eth_get_block_by_number(
-        RpcServices::EthMainnet(None),
+        RpcServices::EthMainnet(Some(rpc_providers)),
         None,
         BlockTag::Latest,
         cycles,
