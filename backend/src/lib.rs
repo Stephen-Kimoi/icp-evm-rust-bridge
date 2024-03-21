830dbf9cf84f639138e96cb46a185672bb5b4a10
Elias Datler <eliasdatler@me.com>
2024-03-21 18:19:36 +0000

    Wip

--- backend/src/lib.rs
+++ backend/src/lib.rs
@@ -1,32 +1,394 @@
-use candid::CandidType;
+#![allow(non_snake_case)]
+use candid::{self, CandidType, Deserialize, Principal};
+use ic_cdk;
 
-#[derive(CandidType)]
-struct Block {
-    baseFeePerGas: u128,
-    difficulty: u128,
-    extraData: String,
-    gasLimit: u128,
-    gasUsed: u128,
-    hash: String,
-    logsBloom: String,
-    miner: String,
-    mixHash: String,
-    nonce: u128,
-    number: u128,
-    parentHash: String,
-    receiptsRoot: String,
-    sha3Uncles: String,
-    size: u128,
-    stateRoot: String,
-    timestamp: u128,
-    totalDifficulty: u128,
-    transactions: Vec<String>,
-    transactionsRoot: Option<String>,
-    uncles: Vec<String>,
+#[derive(CandidType, Deserialize)]
+pub enum Auth {
+    RegisterProvider,
+    FreeRpc,
+    PriorityRpc,
+    Manage,
+}
+
+#[derive(CandidType, Deserialize)]
+pub enum EthSepoliaService {
+    Alchemy,
+    BlockPi,
+    PublicNode,
+    Ankr,
+}
+
+#[derive(CandidType, Deserialize)]
+pub struct HttpHeader {
+    pub value: String,
+    pub name: String,
+}
+
+#[derive(CandidType, Deserialize)]
+pub struct RpcApi {
+    pub url: String,
+    pub headers: Option<Vec<HttpHeader>>,
+}
+
+#[derive(CandidType, Deserialize)]
+pub enum EthMainnetService {
+    Alchemy,
+    BlockPi,
+    Cloudflare,
+    PublicNode,
+    Ankr,
+}
+
+#[derive(CandidType, Deserialize)]
+pub enum RpcServices {
+    EthSepolia(Option<Vec<EthSepoliaService>>),
+    Custom { chainId: u64, services: Vec<RpcApi> },
+    EthMainnet(Option<Vec<EthMainnetService>>),
+}
+
+#[derive(CandidType, Deserialize)]
+pub struct RpcConfig {
+    pub responseSizeEstimate: Option<u64>,
+}
+
+#[derive(CandidType, Deserialize)]
+pub enum BlockTag {
+    Earliest,
+    Safe,
+    Finalized,
+    Latest,
+    Number(u128),
+    Pending,
+}
+
+#[derive(CandidType, Deserialize)]
+pub struct FeeHistoryArgs {
+    pub blockCount: u128,
+    pub newestBlock: BlockTag,
+    pub rewardPercentiles: Option<serde_bytes::ByteBuf>,
+}
+
+#[derive(CandidType, Deserialize)]
+pub struct FeeHistory {
+    pub reward: Vec<Vec<u128>>,
+    pub gasUsedRatio: Vec<f64>,
+    pub oldestBlock: u128,
+    pub baseFeePerGas: Vec<u128>,
+}
+
+#[derive(CandidType, Deserialize, Debug)]
+pub struct JsonRpcError {
+    pub code: i64,
+    pub message: String,
+}
+
+#[derive(CandidType, Deserialize, Debug)]
+pub enum ProviderError {
+    TooFewCycles { expected: u128, received: u128 },
+    MissingRequiredProvider,
+    ProviderNotFound,
+    NoPermission,
+}
+
+#[derive(CandidType, Deserialize, Debug)]
+pub enum ValidationError {
+    CredentialPathNotAllowed,
+    HostNotAllowed(String),
+    CredentialHeaderNotAllowed,
+    UrlParseError(String),
+    Custom(String),
+    InvalidHex(String),
+}
+
+#[derive(CandidType, Deserialize, Debug)]
+pub enum RejectionCode {
+    NoError,
+    CanisterError,
+    SysTransient,
+    DestinationInvalid,
+    Unknown,
+    SysFatal,
+    CanisterReject,
+}
+
+#[derive(CandidType, Deserialize, Debug)]
+pub enum HttpOutcallError {
+    IcError {
+        code: RejectionCode,
+        message: String,
+    },
+    InvalidHttpJsonRpcResponse {
+        status: u16,
+        body: String,
+        parsingError: Option<String>,
+    },
+}
+
+#[derive(CandidType, Deserialize, Debug)]
+pub enum RpcError {
+    JsonRpcError(JsonRpcError),
+    ProviderError(ProviderError),
+    ValidationError(ValidationError),
+    HttpOutcallError(HttpOutcallError),
+}
+
+#[derive(CandidType, Deserialize)]
+pub enum FeeHistoryResult {
+    Ok(Option<FeeHistory>),
+    Err(RpcError),
+}
+
+#[derive(CandidType, Deserialize)]
+pub enum RpcService {
+    EthSepolia(EthSepoliaService),
+    Custom(RpcApi),
+    EthMainnet(EthMainnetService),
+    Chain(u64),
+    Provider(u64),
+}
+
+#[derive(CandidType, Deserialize)]
+pub enum MultiFeeHistoryResult {
+    Consistent(FeeHistoryResult),
+    Inconsistent(Vec<(RpcService, FeeHistoryResult)>),
+}
+
+#[derive(CandidType, Deserialize)]
+pub struct Block {
+    pub miner: String,
+    pub totalDifficulty: u128,
+    pub receiptsRoot: String,
+    pub stateRoot: String,
+    pub hash: String,
+    pub difficulty: u128,
+    pub size: u128,
+    pub uncles: Vec<String>,
+    pub baseFeePerGas: u128,
+    pub extraData: String,
+    pub transactionsRoot: Option<String>,
+    pub sha3Uncles: String,
+    pub nonce: u128,
+    pub number: u128,
+    pub timestamp: u128,
+    pub transactions: Vec<String>,
+    pub gasLimit: u128,
+    pub logsBloom: String,
+    pub parentHash: String,
+    pub gasUsed: u128,
+    pub mixHash: String,
+}
+
+#[derive(CandidType, Deserialize)]
+pub enum GetBlockByNumberResult {
+    Ok(Block),
+    Err(RpcError),
+}
+
+#[derive(CandidType, Deserialize)]
+pub enum MultiGetBlockByNumberResult {
+    Consistent(GetBlockByNumberResult),
+    Inconsistent(Vec<(RpcService, GetBlockByNumberResult)>),
+}
+
+#[derive(CandidType, Deserialize)]
+pub struct GetLogsArgs {
+    pub fromBlock: Option<BlockTag>,
+    pub toBlock: Option<BlockTag>,
+    pub addresses: Vec<String>,
+    pub topics: Option<Vec<Vec<String>>>,
+}
+
+#[derive(CandidType, Deserialize)]
+pub struct LogEntry {
+    pub transactionHash: Option<String>,
+    pub blockNumber: Option<u128>,
+    pub data: String,
+    pub blockHash: Option<String>,
+    pub transactionIndex: Option<u128>,
+    pub topics: Vec<String>,
+    pub address: String,
+    pub logIndex: Option<u128>,
+    pub removed: bool,
+}
+
+#[derive(CandidType, Deserialize)]
+pub enum GetLogsResult {
+    Ok(Vec<LogEntry>),
+    Err(RpcError),
+}
+
+#[derive(CandidType, Deserialize)]
+pub enum MultiGetLogsResult {
+    Consistent(GetLogsResult),
+    Inconsistent(Vec<(RpcService, GetLogsResult)>),
+}
+
+#[derive(CandidType, Deserialize)]
+pub struct GetTransactionCountArgs {
+    pub address: String,
+    pub block: BlockTag,
+}
+
+#[derive(CandidType, Deserialize)]
+pub enum GetTransactionCountResult {
+    Ok(u128),
+    Err(RpcError),
+}
+
+#[derive(CandidType, Deserialize)]
+pub enum MultiGetTransactionCountResult {
+    Consistent(GetTransactionCountResult),
+    Inconsistent(Vec<(RpcService, GetTransactionCountResult)>),
+}
+
+#[derive(CandidType, Deserialize)]
+pub struct TransactionReceipt {
+    pub to: String,
+    pub status: u128,
+    pub transactionHash: String,
+    pub blockNumber: u128,
+    pub from: String,
+    pub logs: Vec<LogEntry>,
+    pub blockHash: String,
+    pub r#type: String,
+    pub transactionIndex: u128,
+    pub effectiveGasPrice: u128,
+    pub logsBloom: String,
+    pub contractAddress: Option<String>,
+    pub gasUsed: u128,
+}
+
+#[derive(CandidType, Deserialize)]
+pub enum GetTransactionReceiptResult {
+    Ok(Option<TransactionReceipt>),
+    Err(RpcError),
+}
+
+#[derive(CandidType, Deserialize)]
+pub enum MultiGetTransactionReceiptResult {
+    Consistent(GetTransactionReceiptResult),
+    Inconsistent(Vec<(RpcService, GetTransactionReceiptResult)>),
+}
+
+#[derive(CandidType, Deserialize)]
+pub enum SendRawTransactionStatus {
+    Ok,
+    NonceTooLow,
+    NonceTooHigh,
+    InsufficientFunds,
+}
+
+#[derive(CandidType, Deserialize)]
+pub enum SendRawTransactionResult {
+    Ok(SendRawTransactionStatus),
+    Err(RpcError),
+}
+
+#[derive(CandidType, Deserialize)]
+pub enum MultiSendRawTransactionResult {
+    Consistent(SendRawTransactionResult),
+    Inconsistent(Vec<(RpcService, SendRawTransactionResult)>),
+}
+
+#[derive(CandidType, Deserialize)]
+pub struct Metrics {
+    pub cyclesWithdrawn: u128,
+    pub responses: Vec<((String, String, String), u64)>,
+    pub errNoPermission: u64,
+    pub inconsistentResponses: Vec<((String, String), u64)>,
+    pub cyclesCharged: Vec<((String, String), u128)>,
+    pub requests: Vec<((String, String), u64)>,
+    pub errHttpOutcall: Vec<((String, String), u64)>,
+    pub errHostNotAllowed: Vec<(String, u64)>,
+}
+
+#[derive(CandidType, Deserialize)]
+pub struct ProviderView {
+    pub cyclesPerCall: u64,
+    pub owner: Principal,
+    pub hostname: String,
+    pub primary: bool,
+    pub chainId: u64,
+    pub cyclesPerMessageByte: u64,
+    pub providerId: u64,
+}
+
+#[derive(CandidType, Deserialize)]
+pub struct ManageProviderArgs {
+    pub service: Option<RpcService>,
+    pub primary: Option<bool>,
+    pub providerId: u64,
+}
+
+#[derive(CandidType, Deserialize)]
+pub struct RegisterProviderArgs {
+    pub cyclesPerCall: u64,
+    pub credentialPath: String,
+    pub hostname: String,
+    pub credentialHeaders: Option<Vec<HttpHeader>>,
+    pub chainId: u64,
+    pub cyclesPerMessageByte: u64,
+}
+
+#[derive(CandidType, Deserialize)]
+pub enum RequestResult {
+    Ok(String),
+    Err(RpcError),
+}
+
+#[derive(CandidType, Deserialize)]
+pub enum RequestCostResult {
+    Ok(u128),
+    Err(RpcError),
+}
+
+#[derive(CandidType, Deserialize)]
+pub struct UpdateProviderArgs {
+    pub cyclesPerCall: Option<u64>,
+    pub credentialPath: Option<String>,
+    pub hostname: Option<String>,
+    pub credentialHeaders: Option<Vec<HttpHeader>>,
+    pub primary: Option<bool>,
+    pub cyclesPerMessageByte: Option<u64>,
+    pub providerId: u64,
 }
 
 #[ic_cdk::query]
-fn getLatestEthereumBlock() -> Block {
+async fn getLatestEthereumBlock() -> Block {
+    let evm_rpc = Principal::from_text("7hfb6-caaaa-aaaar-qadga-cai").unwrap();
+
+    // Define request parameters
+    let params = (
+        &RpcService::Chain(1), // Ethereum mainnet
+        "{\"jsonrpc\":\"2.0\",\"method\":\"eth_gasPrice\",\"params\":null,\"id\":1}".to_string(),
+        1000 as u64,
+    );
+
+    // Get cycles cost
+    let (cycles_result,): (Result<u128, RpcError>,) =
+        ic_cdk::api::call::call(evm_rpc, "requestCost", params.clone())
+            .await
+            .unwrap();
+    let cycles = cycles_result
+        .unwrap_or_else(|e| ic_cdk::trap(&format!("error in `request_cost`: {:?}", e)));
+
+    // Call with expected number of cycles
+    let (result,): (Result<String, RpcError>,) =
+        ic_cdk::api::call::call_with_payment128(evm_rpc, "request", params, cycles)
+            .await
+            .unwrap();
+    match result {
+        Ok(response) => {
+            // Check response structure around gas price
+            assert_eq!(
+                &response[..36],
+                "{\"id\":1,\"jsonrpc\":\"2.0\",\"result\":\"0x"
+            );
+            assert_eq!(&response[response.len() - 2..], "\"}");
+        }
+        Err(err) => ic_cdk::trap(&format!("error in `request` with cycles: {:?}", err)),
+    }
+
     Block {
         baseFeePerGas: 420,
         difficulty: 12,
