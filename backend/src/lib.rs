5dededd0eaee7726bf2e3d944513737b363f19e2
Elias Datler <elias.datler@dfinity.org>
2024-03-29 13:56:41 +0900

    Minor refactor

--- backend/src/lib.rs
+++ backend/src/lib.rs
@@ -3,21 +3,18 @@
 mod evm_rpc;
 use evm_rpc::{
     Block, BlockTag, EthMainnetService, EvmRpcCanister, GetBlockByNumberResult,
-    MultiGetBlockByNumberResult, RpcServices,
+    MultiGetBlockByNumberResult, RpcApi, RpcServices,
 };
 
 #[ic_cdk::update]
 async fn getLatestEthereumBlock() -> Block {
     let cycles = 10_000_000_000;
-    let rpc_providers = vec![EthMainnetService::Cloudflare];
-    let (result,) = EvmRpcCanister::eth_get_block_by_number(
-        RpcServices::EthMainnet(Some(rpc_providers)),
-        None,
-        BlockTag::Latest,
-        cycles,
-    )
-    .await
-    .expect("Call failed");
+    let rpc_providers = RpcServices::EthMainnet(Some(vec![EthMainnetService::Cloudflare]));
+
+    let (result,) =
+        EvmRpcCanister::eth_get_block_by_number(rpc_providers, None, BlockTag::Latest, cycles)
+            .await
+            .expect("Call failed");
 
     match result {
         MultiGetBlockByNumberResult::Consistent(r) => match r {
