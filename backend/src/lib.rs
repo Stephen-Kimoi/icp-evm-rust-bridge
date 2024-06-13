b23644a82a0249f45afb589d88723c5ed90be72b
Stephen-Kimoi <moenisteve@gmail.com>
2024-06-13 20:06:59 +0300

    getting ethereum address for the canister

--- backend/src/lib.rs
+++ backend/src/lib.rs
@@ -1,7 +1,7 @@
 mod evm_rpc;
 mod eth_call;
 use eth_call::call_smart_contract;
-use ethers_core::abi::Token;
+use ethers_core::{abi::{Address, Token}, k256::elliptic_curve::{sec1::ToEncodedPoint, PublicKey}, utils::keccak256};
 use evm_rpc::{
     Block, BlockTag, EthMainnetService, EvmRpcCanister, GetBlockByNumberResult,
     MultiGetBlockByNumberResult, RpcServices,
@@ -10,6 +10,7 @@ use ic_cdk::api::management_canister::ecdsa::{
     ecdsa_public_key, sign_with_ecdsa, EcdsaCurve, EcdsaKeyId, EcdsaPublicKeyArgument,
     EcdsaPublicKeyResponse, SignWithEcdsaArgument, SignWithEcdsaResponse,
 };
+use k256::Secp256k1;
 
 #[ic_cdk::update]
 async fn get_latest_ethereum_block() -> Block {
@@ -43,6 +44,22 @@ async fn get_ecdsa_public_key() -> EcdsaPublicKeyResponse {
     pub_key
 }
 
+#[ic_cdk::update] 
+async fn get_canister_eth_address() -> String {
+    let res = get_ecdsa_public_key().await; 
+    let pubkey = res.public_key; 
+
+    let key: PublicKey<Secp256k1> = PublicKey::from_sec1_bytes(&pubkey)
+        .expect("Failed to pass the public key as SEC1"); 
+    let point = key.to_encoded_point(false); 
+    let point_bytes = point.as_bytes(); 
+    assert_eq!(point_bytes[0], 0x04); 
+    let hash = keccak256(&point_bytes[1..]); 
+    let self_address = ethers_core::utils::to_checksum(&Address::from_slice(&hash[12..32]), None); 
+
+    self_address
+}
+
 #[ic_cdk::update]
 async fn sign_hash_with_ecdsa(message_hash: Vec<u8>) -> SignWithEcdsaResponse {
     let (signature,) = sign_with_ecdsa(SignWithEcdsaArgument {
@@ -55,77 +72,107 @@ async fn sign_hash_with_ecdsa(message_hash: Vec<u8>) -> SignWithEcdsaResponse {
     signature
 }
 
+
+// FUNCTIONS FOR CALLING THE SMART CONTRACT
+const CONTRACT_ADDRESS: &str = "0xAed5d7b083ad30ad6B50f698427aD4907845AAc3";
+
+const ABI_JSON: &str = r#"
+   [
+        {
+            "inputs": [],
+            "stateMutability": "nonpayable",
+            "type": "constructor"
+        },
+        {
+            "inputs": [],
+            "name": "decreaseCount",
+            "outputs": [],
+            "stateMutability": "nonpayable",
+            "type": "function"
+        },
+        {
+            "inputs": [],
+            "name": "getCount",
+            "outputs": [
+                {
+                "internalType": "uint256",
+                "name": "",
+                "type": "uint256"
+                }
+            ],
+            "stateMutability": "view",
+            "type": "function"
+        },
+        {
+            "inputs": [],
+            "name": "increaseCount",
+            "outputs": [],
+            "stateMutability": "nonpayable",
+            "type": "function"
+        }
+    ]
+"#;
+
+fn get_abi() -> ethers_core::abi::Contract {
+    serde_json::from_str::<ethers_core::abi::Contract>(ABI_JSON)
+        .expect("Failed to parse ABI")
+}
+
 #[ic_cdk::update]
-async fn call_increase_count() -> u64 {
-    // let contract_address = "0xAed5d7b083ad30ad6B50f698427aD4907845AAc3".to_string();
-
-    let abi_json = r#"
-       [
-            {
-                "inputs": [],
-                "stateMutability": "nonpayable",
-                "type": "constructor"
-            },
-            {
-                "inputs": [],
-                "name": "decreaseCount",
-                "outputs": [],
-                "stateMutability": "nonpayable",
-                "type": "function"
-            },
-            {
-                "inputs": [],
-                "name": "getCount",
-                "outputs": [
-                    {
-                    "internalType": "uint256",
-                    "name": "",
-                    "type": "uint256"
-                    }
-                ],
-                "stateMutability": "view",
-                "type": "function"
-            },
-            {
-                "inputs": [],
-                "name": "increaseCount",
-                "outputs": [],
-                "stateMutability": "nonpayable",
-                "type": "function"
-            }
-        ]
-    "#; 
-
-    let abi = serde_json::from_str::<ethers_core::abi::Contract>(abi_json).expect("Should serialize"); 
-    
-    // Call the increaseCount function
-    let _: Vec<Token> = call_smart_contract(
-        "0xAed5d7b083ad30ad6B50f698427aD4907845AAc3".to_string(), 
-        &abi, 
-        "increaseCount", 
-        &[], 
-        "latest",
-    ).await; 
-
-    // Call the getCount function to retrieve the updated count
-    let count: Vec<Token> = call_smart_contract(
-        "0xAed5d7b083ad30ad6B50f698427aD4907845AAc3".to_string(),
+async fn call_increase_count() -> String {
+    let abi = get_abi();
+    call_smart_contract(
+        CONTRACT_ADDRESS.to_string(),
         &abi,
-        "getCount",
+        "increaseCount",
         &[],
         "latest",
-    ).await;
+    )
+    .await; 
 
-    // Extract the count value from the returned tokens 
+    return "Increased count".to_string()
+}
+
+#[ic_cdk::update]
+async fn get_count() -> u64 {
+    // Get the ABI
+    let abi = get_abi();
+    
+    // Call the getCount function
+    let count: Vec<Token> = call_smart_contract(
+        CONTRACT_ADDRESS.to_string(), 
+        &abi, 
+        "getCount", 
+        &[], 
+        "latest", 
+    ).await; 
+
     let count_value = count
         .get(0)
         .expect("Expected a single value in the return value")
         .clone() 
         .into_uint()
         .expect("Expected a uint256 value"); 
-    
-    count_value.low_u64()
 
+    count_value.low_u64()
+}
+
+#[ic_cdk::update]
+async fn call_decrease_count() -> String {
+    // Get the ABI
+    let abi = get_abi();
+
+    // Call the decreaseCount function
+    call_smart_contract(
+        CONTRACT_ADDRESS.to_string(), 
+        &abi, 
+        "decreaseCount", 
+        &[], 
+        "latest", 
+    ).await; 
+    // .map_err(|err| format!("Error calling decreaseCount: {err:?}"))?; 
+
+    return "Decreased count".to_string();
 }
 
 fn key_id() -> EcdsaKeyId {
