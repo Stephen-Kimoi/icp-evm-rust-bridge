6bd9c4b740a852f309a4d610d7076880e7fdc392
Stephen-Kimoi <moenisteve@gmail.com>
2024-08-19 19:01:39 +0300

    fixing bash script issues

--- install_and_deploy.sh
+++ install_and_deploy.sh
@@ -84,7 +84,10 @@ echo
 
 # Locally deploy the `evm_rpc` canister
 styled_echo $YELLOW "🚀 Deploying all canisters..."
-dfx deps pull && dfx deps init evm_rpc --argument '(record { nodesInSubnet = 28 })' && dfx deps deploy && dfx deploy
+dfx deps pull 
+dfx deps init evm_rpc --argument '(record { nodesInSubnet = 28 })' 
+dfx deps deploy 
+dfx deploy
 check_status
 styled_echo $GREEN "✅ All canisters deployed locally..."
 echo
